58a379c28f8f05665838f24af02d1227331892ed
tux-rampage <tux-rampage@users.noreply.github.com>
2018-01-23 07:25:26 +0100

    Add missing class

--- src/Api/Middleware/JsonResponseMiddleware.php
+++ src/Api/Middleware/JsonResponseMiddleware.php
@@ -2,6 +2,11 @@
 /**
  * @author    Axel Helmert <ah@luka.de>
  * @license   LUKA Proprietary
- * @copyright Copyright (c) 2017 LUKA netconsult GmbH (www.luka.de)
+ * @copyright Copyright (c) 2018 LUKA netconsult GmbH (www.luka.de)
  */
 
+namespace Rampage\Nexus\Api\Middleware;
+
+class JsonResponseMiddleware
+{
+}
